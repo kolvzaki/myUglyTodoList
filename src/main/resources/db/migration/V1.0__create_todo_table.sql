f2a8d3297e70ff2d35349089472617dc56538f63
Kolvzaki <59278815+kolvzaki@users.noreply.github.com>
2022-08-06 23:45:50 +0800

    Update CI/CD file and change datasource to postgresql

--- src/main/resources/db/migration/V1.0__create_todo_table.sql
+++ src/main/resources/db/migration/V1.0__create_todo_table.sql
@@ -1,6 +1,21 @@
-CREATE TABLE if NOT EXISTS todo
+create sequence todo_sequence start 1;
+
+create table if not exists todo
 (
-    id   int  not null auto_increment primary key,
-    text varchar(255) not null,
-    done boolean
-);
+    id
+    integer
+    generated
+    by
+    default as
+    identity,
+    text
+    varchar
+(
+    255
+) not null,
+    done boolean default false
+    UNIQUE
+(
+    id
+)
+    );
